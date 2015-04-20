45afc4089947bfa9c704b006c4d0c8b305d5fbf8
Paul <student@epicodus.com>
2015-04-20 15:40:40 -0700

    initial commit

---
+++ lib/pingpong.rb
@@ -0,0 +1,18 @@
+class Fixnum
+  define_method(:pingpong) do
+    output = []
+    (1..self).each() do |i|
+      if i % 15 == 0
+        output.push("ping-pong")        
+      elsif i % 3 == 0
+        output.push("ping")
+      elsif i % 5 == 0
+        output.push("pong")
+      else
+        output.push(i)
+      end
+  end
+  puts(output.join(" "))
+  output
+ end
+end
---
+++ spec/ping-pong_spec.rb
@@ -0,0 +1,14 @@
+#english spec
+#The ping-pong method counts from 1 to a given number,
+#(30 for now),
+#replacing nums divis by 3 with ping, 5 by pong,
+#3 and 5 by ping-pong
+
+require ('rspec')
+require('pingpong')
+
+describe('pingpong') do
+  it("if num is divis by 3 & 5") do
+    expect((15).pingpong).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
+  end
+end
